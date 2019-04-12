bda362ef6135e8cb294aac89279bcdb256dd7685
JackCc <1324659902@qq.com>
2019-04-12 11:39:55 +0800

    登陆界面

---
+++ Login/logindialog.cpp
@@ -0,0 +1,34 @@
+﻿#include "logindialog.h"
+#include "ui_logindialog.h"
+#include "qmessagebox.h"
+
+
+LoginDialog::LoginDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginDialog)
+{
+    ui->setupUi(this);
+}
+
+LoginDialog::~LoginDialog()
+{
+    delete ui;
+}
+
+void LoginDialog::on_LoginButton_clicked()
+{   /* 判断用户及密码是否正确 */
+    if((ui->userLineEdit->text().trimmed() == tr("JackCc")) &&  ui->pwdLineEdit->text() == tr("SX1701203"))
+    {
+        accept();
+    }
+    else
+    {
+        QMessageBox::warning(this, tr("Warning"),
+                           tr("User name or Password Error!"),
+                           QMessageBox::Yes);
+        // 清空内容并定位光标
+        ui->userLineEdit->clear();
+        ui->pwdLineEdit->clear();
+        ui->userLineEdit->setFocus();
+    }
+}
